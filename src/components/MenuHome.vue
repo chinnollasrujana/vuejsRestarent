c44735f260e7ece8a413ff60e10dc872e7e80a35
Praveen kumar Bandi <167025759+chinnollasrujana@users.noreply.github.com>
2024-04-20 01:55:05 -0500

    updsted code

--- src/components/MenuHome.vue
+++ src/components/MenuHome.vue
@@ -1,145 +1,171 @@
 <template>
   <div class="main">
-
- <ul class="cards">
-   
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/menuimages.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Non Veg Appetizers</h2>
-       </div>
-     </div>
-   </li>
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/chiken-menchurya.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Veg Appetizers</h2>
-      
-       </div>
-     </div>
-   </li>
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/checken_65.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Non Veg Main Course </h2>
-       </div>
-     </div>
-   </li>
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/Pepper_Chicken-Dry.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Veg Main Course</h2>
-       </div>
-     </div>
-   </li>
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/Curry-Leaf_Chicken.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Biryani, Fried Rice & More</h2>
-       </div>
-     </div>
-   </li>
-   <li class="cards_item">
-     <div class="card">
-       <div class="card_image"><img src="../assets/images/menuimages/Chilli-Shrimp.jpeg"></div>
-       <div class="card_content">
-         <h2 class="card_title">Desserts</h2>
-       </div>
-     </div>
-   </li>
- </ul>
-</div>
-
+    <div class="filters">
+      <ul class="cards">
+        <li
+          class="cards_item"
+          v-for="(category, index) in categories"
+          :key="index"
+        >
+          <div class="card">
+            <div class="card_image">
+              <img :src="'../src/assets/images/menuimages/'+category.image_url">
+            </div>
+            <div class="card_content">
+              <h2 class="card_title">{{ category.category_name }}</h2>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <button class="filter-button show-all-button" @click="showAll(category)">
+        Show All
+      </button>
+    </div>
+  </div>
 </template>
 <script>
-export default{
-  name: 'Header'
+export default {
+  name: "Header",
+  data() {
+    return {
+      selectedCategory: null,
+      categories: [
+        {
+          id: 1,
+          category_name: "Non Veg",
+          image_url: "Pepper_Chicken-Dry.jpeg",
+        },
+        {
+          id: 2,
+          category_name: "Veg",
+          image_url: "chana_masala.jpg",
+        },
+        {
+          id: 3,
+          category_name: "Biryani",
+          image_url: "shrimp_biryani.jpg",
+        },
+        {
+          id: 4,
+          category_name: "Appetizers",
+          image_url: "Samosa-apts.png",
+        }
+      ],
+      methods: {
+        filterFoodItems(category) {
+          // Set selectedArticle data to
+          // selected category's name
+          this.selectedArticle = category;
+        },
+        // set selectedArticle data to null
+        showAll() {
+          this.selectedArticle = null;
+        },
+      },
+    }
+  }
 }
-
-
 </script>
 
 <style>
-
-
-.main{
-
- max-width: 1200px;
- margin: 20px auto;
+.main {
+  max-width: 1200px;
+  margin: 20px auto;
 }
 
 h1 {
-   font-size: 24px;
-   font-weight: 400;
-   text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+  text-align: center;
 }
 
 img {
- height: auto;
- max-width: 100%;
- vertical-align: middle;
+  height: auto;
+  max-width: 100%;
+  vertical-align: middle;
 }
-
-
+.filters .cards{
+  cursor: pointer;
+}
 .cards {
- display: flex;
- flex-wrap: wrap;
- list-style: none;
- margin: 0;
- padding: 20%;
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 20%;
 }
 
 .cards_item {
- display: flex;
- padding: 1rem;
+  display: flex;
+  padding: 1rem;
 }
 
 @media (min-width: 40rem) {
- .cards_item {
-   width: 50%;
- }
+  .cards_item {
+    width: 20% !important;
+  }
 }
 
 @media (min-width: 56rem) {
- .cards_item {
-   width: 33.3333%;
- }
+  .cards_item {
+    width: 25% !important;
+  }
 }
 
 .card {
- background-color: white;
- border-radius: 0.25rem;
- box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
- display: flex;
- flex-direction: column;
- overflow: hidden;
+  background-color: white;
+  border-radius: 0.25rem;
+  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 }
 
 .card_content {
- padding: 1rem;
- /* background: linear-gradient(to bottom left, #EF8D9C 40%, #FFC39E 100%); */
+  padding: 1rem;
+  /* background: linear-gradient(to bottom left, #EF8D9C 40%, #FFC39E 100%); */
 }
 
 .card_title {
- color: #000;
- font-size: 1.1rem;
- font-weight: 700;
- letter-spacing: 1px;
- text-transform: capitalize;
- margin: 0px;
+  color: #000;
+  font-size: 1.1rem;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 0px;
 }
 
 .card_text {
- color: #000;
- font-size: 0.875rem;
- line-height: 1.5;
- margin-bottom: 1.25rem;    
- font-weight: 400;
+  color: #000;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  margin-bottom: 1.25rem;
+  font-weight: 400;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
-  </style>
+.item {
+  flex: 0 32%;
+  height: 100px;
+  margin-bottom: 2%; /* (100-32*3)/2 */
+}
+.filter-button {
+  text-transform: uppercase;
+  margin: 0 6px;
+  cursor: pointer;
+  font-family: monospace;
+  font-size: 1.2rem;
+  background: rgb(14, 209, 85);
+  padding: 10px;
+  color: #fff;
+  border-radius: 5%;
+}
+
+.show-all-button {
+  background: rgb(5, 91, 15);
+}
+</style>
